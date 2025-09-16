f87a5af2c49f68a82a29d569f56e3726e3f79ce2
TitouSou <titisou14@outlook.fr>
2025-09-16 08:51:55 +0200

    Ex 1

--- script/app.js
+++ script/app.js
@@ -1,4 +1,12 @@
-let texte = "Hello World !";
-document.getElementById("var1").innerHTML = texte;
-console.log(texte);
+// Ex 0
 
+// let texte = "Hello World !";
+// document.getElementById("var1").innerHTML = texte;
+// console.log(texte);
+
+// Ex 1 
+
+let temp = 10;
+let temp2 = temp * 9/5 + 32;
+console.log(temp);
+console.log(temp2);
